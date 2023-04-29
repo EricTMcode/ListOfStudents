5ac0f2713a8bd595f2d4b4675acd0b3be3ab1e51
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-04-29 14:45:46 +0200

    - create students array - iterate through students array and print all the name in a list with a swift system image - add a navigation title

--- ListOfStudents/View/ContentView.swift
+++ ListOfStudents/View/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Holt B.",
+                    "Luke B.",
+                    "Michael B.",
+                    "Milan C.",
+                    "Russell C.",
+                    "Gabriela C.",
+                    "Kevin C.",
+                    "Charles C.",
+                    "Matthew C.",
+                    "Gleidson D.",
+                    "Jack D.",
+                    "John G.",
+                    "Matthew Gr.",
+                    "Matthew Gu.",
+                    "William H.",
+                    "Shane J.",
+                    "Connor J.",
+                    "Andy J.",
+                    "Christopher K.",
+                    "Dylan K.",
+                    "Riya K.",
+                    "Aakash K.",
+                    "Benjamin L.",
+                    "Dilan L.",
+                    "Anthony M.",
+                    "Tyler M.",
+                    "Annie M.",
+                    "Avery M.",
+                    "Emily M.",
+                    "Addison M.",
+                    "Dylan M.",
+                    "Owen O.",
+                    "Zachary P.",
+                    "Gabriella P.",
+                    "William P.",
+                    "Caitlin P.",
+                    "Syed R.",
+                    "William R.",
+                    "Lauren S.",
+                    "George S.",
+                    "Astrid S.",
+                    "Thomas S.",
+                    "Hailey S.",
+                    "Jasmine T.",
+                    "Michael V.",
+                    "Jonathan W.",
+                    "Theodore W.",
+                    "Andrew Y."
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(students, id: \.self) { student in
+                    HStack {
+                        Image(systemName: "swift")
+                            .foregroundColor(.orange)
+                        Text(student)
+                    }
+                    
+                }
+            }
+            .font(.title2)
+            .listStyle(.plain)
+            .navigationTitle("Swifters!")
         }
-        .padding()
     }
 }
 
