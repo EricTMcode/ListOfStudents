fafdb698fdb002ee23589caed836df01d4df9be0
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-04-29 14:36:13 +0200

    Initial Commit

---
+++ ListOfStudents/ListOfStudentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfStudentsApp.swift
+//  ListOfStudents
+//
+//  Created by Eric on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListOfStudentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
