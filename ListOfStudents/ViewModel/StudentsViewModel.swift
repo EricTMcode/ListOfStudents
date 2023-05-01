4f996f58a5aee3cd60d456c72c6aed2d8144b246
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-01 13:50:11 +0200

    - Save data to an iOS Device directory - End of the challenge

--- ListOfStudents/ViewModel/StudentsViewModel.swift
+++ ListOfStudents/ViewModel/StudentsViewModel.swift
@@ -11,59 +11,7 @@ class StudentsViewModel: ObservableObject {
     @Published var studentArray: [Student] = []
     
     init() {
-        let students = ["Holt B.",
-                        "Luke B.",
-                        "Michael B.",
-                        "Milan C.",
-                        "Russell C.",
-                        "Gabriela C.",
-                        "Kevin C.",
-                        "Charles C.",
-                        "Matthew C.",
-                        "Gleidson D.",
-                        "Jack D.",
-                        "John G.",
-                        "Matthew Gr.",
-                        "Matthew Gu.",
-                        "William H.",
-                        "Shane J.",
-                        "Connor J.",
-                        "Andy J.",
-                        "Christopher K.",
-                        "Dylan K.",
-                        "Riya K.",
-                        "Aakash K.",
-                        "Benjamin L.",
-                        "Dilan L.",
-                        "Anthony M.",
-                        "Tyler M.",
-                        "Annie M.",
-                        "Avery M.",
-                        "Emily M.",
-                        "Addison M.",
-                        "Dylan M.",
-                        "Owen O.",
-                        "Zachary P.",
-                        "Gabriella P.",
-                        "William P.",
-                        "Caitlin P.",
-                        "Syed R.",
-                        "William R.",
-                        "Lauren S.",
-                        "George S.",
-                        "Astrid S.",
-                        "Thomas S.",
-                        "Hailey S.",
-                        "Jasmine T.",
-                        "Michael V.",
-                        "Jonathan W.",
-                        "Theodore W.",
-                        "Andrew Y."
-        ]
-        
-        for student in students {
-            studentArray.append(Student(id: UUID().uuidString, name: student))
-        }
+        loadData()
     }
     
     func saveStudent(student: Student) {
@@ -76,13 +24,38 @@ class StudentsViewModel: ObservableObject {
                 studentArray[index] = student
             }
         }
+        saveData()
     }
     
     func deleteStudent(indexSet: IndexSet) {
         studentArray.remove(atOffsets: indexSet)
+        saveData()
     }
     
     func moveStudent(fromOffsets: IndexSet, toOffset: Int) {
         studentArray.move(fromOffsets: fromOffsets, toOffset: toOffset)
+        saveData()
+    }
+    
+    //MARK: - DOCUMENTS DIRECTORY
+    
+    let path = URL.documentsDirectory.appending(component: "studentArray")
+    
+    func saveData() {
+        let data = try? JSONEncoder().encode(studentArray)
+        do {
+            try data?.write(to: path)
+        } catch {
+            print("😡 ERROR: Could not save data \(error.localizedDescription)")
+        }
+    }
+    
+    func loadData() {
+        guard let data = try? Data(contentsOf: path) else { return }
+        do {
+            studentArray = try JSONDecoder().decode([Student].self, from: data)
+        } catch {
+            print("😡 ERROR: Could not load data \(error.localizedDescription)")
+        }
     }
 }
